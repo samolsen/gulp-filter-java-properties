62ac23f30bc4eb37963b4efee5cf9eb45dadb3da
Sam Olsen <olsensh@gmail.com>
2014-12-28 22:03:27 -0800

    Add tests for accepting delimiters

--- test/main.js
+++ test/main.js
@@ -94,5 +94,81 @@ describe("gulp-filter-java-properties", function () {
 			stream.end();
 		});
 	});	
+
+	describe('custom delimiters', function() {
+		
+
+		var expectedFile = new gutil.File({
+			path: "test/expected/custom-delimiters.txt",
+			cwd: "test/",
+			base: "test/expected",
+			contents: fs.readFileSync("test/expected/custom-delimiters.txt")
+		});
+
+		var stream;
+		beforeEach(function () {
+			stream = filterJavaProperties({
+				propertiesPath: path.resolve(__dirname, "fixtures/configure.properties"),
+				delimiters: '(*)'
+			});
+		});
+
+		it("should produce expected file via buffer", function (done) {
+
+			var srcFile = new gutil.File({
+				path: "test/fixtures/template.txt",
+				cwd: "test/",
+				base: "test/fixtures",
+				contents: fs.readFileSync("test/fixtures/template.txt")
+			});	
+
+			stream.on("error", function(err) {
+				should.exist(err);
+				done(err);
+			});
+
+			stream.on("data", function (newFile) {
+
+				should.exist(newFile);
+				should.exist(newFile.contents);
+
+				String(newFile.contents).should.equal(String(expectedFile.contents));
+				done();
+			});
+
+			stream.write(srcFile);
+			stream.end();
+		});
+
+		it("should produce expected file via stream", function (done) {
+
+			var srcFile = new gutil.File({
+				path: "test/fixtures/template.txt",
+				cwd: "test/",
+				base: "test/fixtures",
+				contents: fs.createReadStream("test/fixtures/template.txt")
+			});
+
+			stream.on("error", function(err) {
+				should.exist(err);
+				done();
+			});
+
+			stream.on("data", function (newFile) {
+
+				should.exist(newFile);
+				should.exist(newFile.contents);
+
+				newFile.contents.pipe(es.wait(function(err, data) {
+					should.not.exist(err);
+					String(data).should.equal(String(expectedFile.contents));
+					done();
+				}));
+			});
+
+			stream.write(srcFile);
+			stream.end();
+		});
+	});	
 	
 });
